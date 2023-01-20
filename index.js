3e6c41aafab857e89444a621fc4b2feebe8cd894
osjarju <115750838+osjarju@users.noreply.github.com>
2023-01-19 21:48:51 -0800

    codes working without errors

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 //import express
-const server = require('express');
+const express = require('express');
+
+const path = require('path');
 
 //import routes 
 const api = require("./routes/indexRoutes")
@@ -8,12 +10,22 @@ const api = require("./routes/indexRoutes")
 const PORT = 3001;
 
 //init app
-const app = server();
+const app = express();
 
 //middleware
-app.use(server.urlencoded());
-app.use(server.json());
+app.use(express.urlencoded());
+app.use(express.json());
 app.use("/api", api);
+
+app.use(express.static('./public'))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
 
 const printPort = () => {
     console.log(`listening to port ${PORT}`)
--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -1,10 +1,21 @@
 //import
 const express = require("express");
 
+const notes = require('../notes.json');
+
 //acquire route files
 const fileNameRoute = require("./fileNames");
 
 const app = express();
+
+app.get('/notes', (req, res) => {
+    res.json(notes)
+})
+
+app.post('/notes', (req, res) => {
+    console.log(req.body)
+    res.json(notes)
+})
 
 //link routes to app
 app.use("/fileName", fileNameRoute);
