060de1cbda20bd8f9b6acfe4e2f040264dd75e76
osjarju <115750838+osjarju@users.noreply.github.com>
2023-01-23 23:59:48 -0800

    ADD and DELETE not working

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -1,6 +1,6 @@
 //imports
 const express = require("express").Router();
-const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const { readFromFile, readAndAppend, writeToFile } = require('../helpers/fsUtils');
 const uuidv1 = require('uuid/v1');
 
 //GET Route 
@@ -21,11 +21,45 @@ express.post('/notes', (req, res) => {
     };
 
     readAndAppend(newNote, './db/fileName.json');
-    res.json(`Tip added successfully`);
+    res.json(`Note added successfully`);
   } else {
     res.send('Error reading file');
   }
 
+});
+
+express.delete('/notes', (req, res) => {
+  console.log(req.body);
+  const { title, text, id } = req.body;
+  if (req.body) {
+    const noteToDelete = {
+      title,
+      text,
+      id
+    };
+
+    readFromFile(noteToDelete, './db/fileName.json');
+    res.json('Note deleted successfully');
+  } else {
+    res.send('Error deleting file');
+  }
+});
+
+express.put('./notes', (req, res) => {
+  console.log(req.body);
+  const { title, text, id } = req.body;
+  if (req.body) {
+    const noteToAdd = {
+      title,
+      text,
+      id
+    };
+
+    writeToFile(noteToAdd, './db/fileName,json');
+    res.json('Note added successfuly');
+  } else {
+    res.send('Error adding file');
+  }
 });
 
 module.exports = express;
