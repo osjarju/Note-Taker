0bb66bb715dd0a5d951182757e53d9b0d07ed0e4
osjarju <115750838+osjarju@users.noreply.github.com>
2023-01-23 18:18:13 -0800

    POST route working

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -1,44 +1,31 @@
-//import
+//imports
 const express = require("express").Router();
 const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
 const uuidv1 = require('uuid/v1');
 
+//GET Route 
 express.get('/notes', (req, res) => {
-    readFromFile('./db/fileName.json')
-        .then((data) => res.json(JSON.parse(data)));
-})
+  readFromFile('./db/fileName.json')
+    .then((data) => res.json(JSON.parse(data)));
+});
 
+//POST Route 
 express.post('/notes', (req, res) => {
-    let newPost = req.body;
-    if (req.body) {
-        newPost = {
-            express_id: uuidv1(),
-        };
-        return uuidv1;
+  console.log(req.body);
+  const { title, text } = req.body;
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      id: uuidv1(),
+    };
 
-        readAndAppend(newPost, './db/fileName.json');
-        res.json();
-    } else {
-        res.send('Error reading file');
-    }
+    readAndAppend(newNote, './db/fileName.json');
+    res.json(`Tip added successfully`);
+  } else {
+    res.send('Error reading file');
+  }
 
-})
+});
 
-
-// newPost.push(newFileName);
-// fs.writeFile("./db/fileName.json", JSON.stringify(newPost), (err, data) => {
-//         if (err) {
-//             res.send("Error writing to file")
-//         } else {
-//             res.json(
-//                 {
-//                     data: newPost,
-//                     lastAdded: newFileName
-//                 }
-//             )
-//         }
-//     })
-// });
-
-//export
 module.exports = express;
